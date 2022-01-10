e6fd34ded7ec1bb70552d3b974fd14e55244279d
rachana-08 <rachana08@gmail.com>
2022-01-10 18:18:40 +0530

    modules added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views',path.join(__dirname,'views'));
 app.engine('handlebars', handlebars.engine({defaultLayout:'layout'}));
 app.set('view engine','handlebars');
 
-app.use(express.static(path.join(__dirname, 'lib')));
+app.use(express.static(path.join(__dirname, 'lib/public')));
 
 
 app.use(
